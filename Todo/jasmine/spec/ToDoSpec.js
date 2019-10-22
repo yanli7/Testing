e46df3bc99bc5aa72916894e770eb02ba01addfe
YAN <yliy1874@gmail.com>
2019-10-22 17:26:35 +0300

    Update ToDoSpec.js

--- Todo/jasmine/spec/ToDoSpec.js
+++ Todo/jasmine/spec/ToDoSpec.js
@@ -1,7 +1,17 @@
+describe("Testing DOM manipulation", function() {
+  it("should check on the DOM element", function() {
+    let todo = new ToDo();
+    let li = document.createElement("li");
+    let input = document.getElementById("input").value;
+    let txts = document.createTextNode(input);
+    li.appendChild(txts);
+    expect(todo.addNew().li).toEqual(li);
+  });
+});
 describe("Testing the functionalities of the ToDO List", () => {
   it("should validate the 'Enter' keypress", () => {
     let todo = new ToDo();
-    let keyPressed;
+    let keyPressed = null;
 
     function keyPress(key) {
       let event = document.createEvent("Event");
@@ -12,31 +22,7 @@ describe("Testing the functionalities of the ToDO List", () => {
     document.addEventListener("keydown", function(e) {
       keyPressed = e.keyCode;
     });
-
     let press = keyPress(13);
     expect(todo.enterIt()).toEqual(press);
-  });
-
-  it("should check on the DOM element", () => {
-    let todo = new ToDo();
-    let li = document.createElement("li");
-    let input = document.getElementById("input").value;
-    let txts = document.createTextNode(input);
-    li.appendChild(txts);
-    expect(todo.addNew().li).toEqual(li);
-  });
-
-  it("should delete an item", function() {
-    let todo = new ToDo();
-    let result = todo.addNew();
-    
-    let close = document.getElementsByClassName("close");
-    close.addEventListener("click", deleteIt);
-    function deleteIt(e) {
-      let item = e.target.parentNode;
-      let ul = item.parentNode;
-      ul.removeChild(item);
-    }
-    expect(result.close.click()).toEqual(close.click());
   });
 });
